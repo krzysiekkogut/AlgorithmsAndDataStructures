53693bdc3f2ea2164088dee1a44556097bc6f2e3
Krzysztof Kogut <krzysiek.kogut@outlook.com>
2017-12-10 19:35:33 +0100

    AssemblyInfo.cs edit

--- Algorithms/Properties/AssemblyInfo.cs
+++ Algorithms/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Algorithms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Algorithms and Data Structures")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Algorithms")]
